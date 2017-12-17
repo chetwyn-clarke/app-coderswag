0f229f04a6b71612be4782ac3424ddcfd6da794c
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2017-12-17 08:24:28 -0500

    Finished lesson 01

---
+++ Coder Swag/Coder Swag/Views/CategoryCell.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryCell.swift
+//  Coder Swag
+//
+//  Created by Chetwyn on 12/17/17.
+//  Copyright © 2017 Clarke Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
